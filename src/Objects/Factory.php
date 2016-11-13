ee8c0f0ea7385f3524e424db42dc6c66333e45b6
Tomasz Konarski <tomasz@konar.ski>
2016-11-14 00:31:31 +0100

    Added more resiliency to the Machine factory method

--- src/Objects/Factory.php
+++ src/Objects/Factory.php
@@ -36,20 +36,20 @@ class Factory
     {
         return new Machine(
             $properties['name'],
-            $properties['type'],
-            $properties['postcode'],
-            $properties['province'],
-            $properties['street'],
-            $properties['buildingnumber'],
-            $properties['town'],
-            $properties['latitude'],
-            $properties['longitude'],
-            $properties['paymentavailable'] == 't',
-            $properties['operatinghours'],
-            $properties['locationdescription'],
-            $properties['paymentpointdescription'],
-            $properties['partnerid'],
-            $properties['paymenttype']
+            array_key_exists('type', $properties) ? $properties['type'] : Machine::TYPE_PACK_MACHINE,
+            array_key_exists('postcode', $properties) ? $properties['postcode'] : '',
+            array_key_exists('province', $properties) ? $properties['province'] : '',
+            array_key_exists('street', $properties) ? $properties['street'] : '',
+            array_key_exists('buildingnumber', $properties) ? $properties['buildingnumber'] : '',
+            array_key_exists('town', $properties) ? $properties['town'] : '',
+            array_key_exists('latitude', $properties) ? $properties['latitude'] : '',
+            array_key_exists('longitude', $properties) ? $properties['longitude'] : '',
+            array_key_exists('paymentavailable', $properties) ? strcasecmp($properties['paymentavailable'],'t')==0 : false,
+            array_key_exists('operatinghours', $properties) ? $properties['operatinghours'] : '',
+            array_key_exists('locationdescription', $properties) ? $properties['locationdescription'] : '',
+            array_key_exists('paymentpointdescription', $properties) ? $properties['paymentpointdescription'] : '',
+            array_key_exists('partnerid', $properties) ? $properties['partnerid'] : '',
+            array_key_exists('paymenttype', $properties) ? $properties['paymenttype'] : ''
         );
     }
 
